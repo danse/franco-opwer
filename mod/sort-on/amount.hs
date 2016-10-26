ae707449149dd59a1767dbd2705d9183ecbda230
danse <f.occhipinti@gmail.com>
2016-10-26 18:35:10 +0200

    remove a redundant function definition

--- mod/sort-on/amount.hs
+++ mod/sort-on/amount.hs
@@ -3,7 +3,4 @@ import Upwork
 import Data.List (sortOn)
 import Text.Read (readMaybe)
 
-readFloatOrZero :: String -> Float
-readFloatOrZero = (maybe 0 Prelude.id) . readMaybe
-
 main = modifyList (reverse . sortOn (readFloatOrZero . amount))
