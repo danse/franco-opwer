15310e12bff477ad948fbc542b1ba58bedd23cb7
danse <f.occhipinti@gmail.com>
2017-03-27 18:13:41 +0200

    in my daily check, select workload `as_needed` and remove fix priced gigs

--- daily-check.sh
+++ daily-check.sh
@@ -1,3 +1,3 @@
-ls ext/sub/ | while read sub; do for f in flex fixe; do opwer-search ${f}-query ext/sub/$sub ext/sort/newest > results/${f}/ext-sub-$sub; done; done
-for f in flex fixe; do opwer-search ${f}-query ext/cat/data > results/${f}/ext-cat-data; done
+ls ext/sub/ | while read sub; do for f in flex; do opwer-search ${f}-query ext/sub/$sub ext/sort/newest ext/workload/as_needed > results/${f}/ext-sub-$sub; done; done
+for f in flex; do opwer-search ${f}-query ext/cat/data > results/${f}/ext-cat-data; done
 . sanity-check.sh
